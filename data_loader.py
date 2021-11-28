1e0fbe492c9f25364a5fb1094ecf5e25f15ed235
Way-Yuhao <liu697@wisc.edu>
2021-11-27 18:47:49 -0600

    working on dataloader

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,24 @@
-import cv2
-import io
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.tensorboard import SummaryWriter
-import numpy as np
-import os.path as p
-from tqdm import tqdm
-from matplotlib import pyplot as plt
+import torchvision.transforms as T
+from torchvision.io import read_image
+from torch.utils.data import DataLoader, Dataset
 
+
+class PetfinderDataset(Dataset):
+    def __init__(self, df, image_size=224):
+        self._X = df["Id"].values
+        self._y = None
+        if "Pawpularity" in df.keys():
+            self._y = df["Pawpularity"].values
+        self._transform = T.Resize([image_size, image_size])
+
+    def __len__(self):
+        return len(self._X)
+
+    def __getitem__(self, idx):
+        image_path = self._X[idx]
+        image = read_image(image_path)
+        image = self._transform(image)
+        if self._y is not None:
+            label = self._y[idx]
+            return image, label
+        return image
---
+++ train.py
@@ -0,0 +1,42 @@
+import cv2
+import io
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.utils
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+import os
+import os.path as p
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+from data_loader import PetfinderDataset
+import pandas as pd
+
+"""Global Parameters"""
+dataset_path = "/mnt/data1/yl241/datasets/Pawpularity/"
+network_weight_path = "./weight/"
+model_name = None
+version = None
+num_workers_train = 8
+batch_size = 8
+
+
+"""Hyper Parameters"""
+init_lr = 5e-1
+epoch = 2000
+
+
+def load_data(df):
+    data_loader = torch.utils.data.DataLoader(PetfinderDataset(df))
+
+
+def main():
+    df = pd.read_csv(p.join(dataset_path, "train.csv"))
+    df["Id"] = df["Id"].apply(lambda x: os.path.join(dataset_path, "train", x + ".jpg"))
+
+
+
+if __name__ == "__main__":
+    main()
